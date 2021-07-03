bdfca546ba4ea1465518ec46f78450041190baa8
JoelS-76 <84299125+JoelS-76@users.noreply.github.com>
2021-07-03 07:34:23 -0600

    Added candidate votes and pct

--- PyPoll.py
+++ PyPoll.py
@@ -8,15 +8,61 @@ file_to_load = os.path.join("Resources", "election_results.csv")
 #Create a filename variable to a direct or indirect path to the file.
 file_to_save = os.path.join("analysis", "election_analysis.txt")
 
+#Initialize a total vote counter
+total_votes = 0
+
+# Candidate options
+candidate_options = []
+# Declare empty dictionary
+candidate_votes = {}
+
 #Open the election results and read the file.
 with open(file_to_load) as election_data:
 
     #Read the file object with the reader function
     file_reader = csv.reader(election_data)
 
+    #Read the header row
+    headers = next(file_reader)
+
     #Print each row in the CSV file.
     for row in file_reader:
-        print(row)
+        # Add to the total vote count
+        total_votes += 1
+
+        # Print the candidate name from each row
+        candidate_name = row[2]
+
+        # If the candidate does not match any existing candidate
+        if candidate_name not in candidate_options:
+            #Add the candidate name to the candidate list
+            candidate_options.append(candidate_name)
+            # Track that candidate's vote count
+            candidate_votes[candidate_name] = 0
+
+        # Add a vote to that candidate's count
+        candidate_votes[candidate_name] += 1
+
+#Determine vote percentage
+#Iterate through the candidate list
+for candidate_name in candidate_votes:
+    # Retrieve vote count for each candidate
+    votes = candidate_votes[candidate_name]
+    #Calculate pct of votes
+    vote_percentage = float(votes) / float(total_votes) * 100
+    #Print the candidate's name with pct of vote
+    print(f"{candidate_name}: received {vote_percentage:.2f}% of the vote.")
+
+
+# Print the candidate vote dictionary
+print(candidate_votes)
+
+# Print the candidate list
+print(candidate_options)
+
+# Print the total votes
+print(total_votes)
+
 
 
 
@@ -38,8 +84,8 @@ with open(file_to_save, "w") as txt_file:
     
 
 
-# 1. The total number of votes cast
-# 2. A complete list of candidates who recieved votes
+
+
 # 3. The percentage of votes each candidate won
-# 4. The total number of votes each candidate won
+
 # 5. The winner of the election based on popular vote.
